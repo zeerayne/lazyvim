b5bd0e368827e31aa4228f9d053522bd734117ce
Ilya Bogdanov <zeerayne1337@gmail.com>
2025-07-01 14:39:53 +0300

    options: show hidden and ignored files in explorer and picker by default
    Docs
    ----
    ## Explorer
    https://github.com/folke/snacks.nvim/blob/main/docs/explorer.md
    
    ## Files
    https://github.com/folke/snacks.nvim/blob/main/docs/picker.md#files

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,19 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = true,
+        },
+      },
+    },
+    explorer = {
+      files = {
+        hidden = true,
+        ignored = true,
+      },
+    },
+  },
+}
